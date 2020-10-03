0682adda037f0ba0208032e162651a2da54932d0
Erik Aker <eraker@gmail.com>
2020-10-03 08:40:17 -0700

    Add some HTTP methods in advance of routering

---
+++ tests/http/test_methods.py
@@ -0,0 +1,19 @@
+import pytest
+
+from hypothesis import given
+from hypothesis import strategies
+
+from tokamak.http import methods
+
+
+@given(strategies.sampled_from(methods.Method))
+def test_method_prop(meth):
+    assert methods.Method.read(meth.value) is meth
+    assert methods.Method.read(meth.value.lower()) is meth
+
+
+@pytest.mark.parametrize(
+    "val,result", (("CoNnECT", methods.Method.CONNECT), ("", None), (None, None),),
+)
+def test_method_read(val, result):
+    assert methods.Method.read(val) is result
---
+++ tokamak/http/methods.py
@@ -0,0 +1,24 @@
+import enum
+from typing import Optional
+
+
+@enum.unique
+class Method(enum.Enum):
+    CONNECT = "CONNECT"
+    DELETE = "DELETE"
+    HEAD = "HEAD"
+    GET = "GET"
+    OPTIONS = "OPTIONS"
+    PATCH = "PATCH"
+    POST = "POST"
+    PUT = "PUT"
+    TRACE = "TRACE"
+
+    @classmethod
+    def read(cls, value: str) -> Optional["Method"]:
+        if hasattr(value, "upper"):
+            value = value.upper()
+        for elem in Method:
+            if elem.value == value:
+                return elem
+        return None
