d9c21591bb3c1ce1a1f79c441c0f8ae398c399ea
Erik <eraker@gmail.com>
2022-08-16 17:29:39 -0700

    Add Github Actions (#18)
    * Add tests to run on PR
    * Add release script
    * Nominal version-bump to test/demonstrate release post-merge.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,18 +1,9 @@
 #!/bin/sh -e
 
-export PREFIX=""
-if [ -d 'venv' ] ; then
-    export PREFIX="venv/bin/"
-fi
-if [ -d '.venv' ] ; then
-    export PREFIX=".venv/bin/"
-fi
+set -Eeuxo
 export SOURCE_FILES="tokamak tests"
 
-set -x
-
-${PREFIX}pytest
-${PREFIX}black $SOURCE_FILES
-${PREFIX}mypy $SOURCE_FILES
-${PREFIX}isort $SOURCE_FILES
-
+poetry run black --check --diff $SOURCE_FILES
+poetry run isort --check --diff --project=tokamak $SOURCE_FILES
+# poetry run mypy $SOURCE_FILES
+poetry run pytest
