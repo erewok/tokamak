7e60f31e74139d66965f7dd4da17103f7592eb2f
Erik Aker <eraker@gmail.com>
2022-08-16 17:10:17 -0700

    Add a release one.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,5 @@
 #!/bin/sh -e
 
-export PREFIX=""
-if [ -d 'venv' ] ; then
-    export PREFIX="venv/bin/"
-fi
-if [ -d '.venv' ] ; then
-    export PREFIX=".venv/bin/"
-fi
-export SOURCE_FILES="tokamak tests"
-
-set -x
-
 poetry build
-${PREFIX}twine check dist/*
-${PREFIX}twine upload dist/*
+poetry run twine check dist/*
+poetry run twine upload dist/*
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,9 @@
 #!/bin/sh -e
 
-export PREFIX=""
-if [ -d 'venv' ] ; then
-    export PREFIX="venv/bin"
-fi
-if [ -d '.venv' ] ; then
-    export PREFIX=".venv/bin"
-fi
+set -Eeuxo
 export SOURCE_FILES="tokamak tests"
 
-"${PREFIX}/black" --check --diff $SOURCE_FILES
-"${PREFIX}/isort" --check --diff --project=tokamak $SOURCE_FILES
-# "${PREFIX}/mypy" $SOURCE_FILES
-"${PREFIX}/pytest"
+poetry run black --check --diff $SOURCE_FILES
+poetry run isort --check --diff --project=tokamak $SOURCE_FILES
+# poetry run mypy $SOURCE_FILES
+poetry run pytest
